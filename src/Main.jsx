7bc03f5e8a74a187e424648c3e3b425819ed14e9
mejrakucevic <mejrakucevic420@gmail.com>
2024-05-06 19:47:53 +0200

    .

---
+++ src/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+function Main() {
+  return (
+    <body>
+      <div>
+        <div className="container">
+          <h1 className="text-white underline font-bold text-6xl">
+            The last of us
+          </h1>
+          <button className="bg-yellow-950 hover:bg-white">Start</button>
+          <h1 className="bg-yellow-950 text-white font-bold p-2 w-40 h-10 text-center ">
+            Joel
+          </h1>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Main.jsx",
+    "./src/style.css",
+    "./src/App.js",
+    "./src/App.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
