a782675e4aacfcab43b8f58ecc1bc6f0862b8913
mejrakucevic <mejrakucevic420@gmail.com>
2024-05-15 21:41:28 +0200

    .

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,72 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style.css";
 import Quiz from "./Quiz";
 import tlou1 from "../pics/tlou1.jpg";
 
+import tlou2 from "../pics/tlou2.jpg";
+import tlou3 from "../pics/tlou3.jpg";
+
+import tlou5 from "../pics/tlou5.jpg";
+import tlou6 from "../pics/tlou6.jpg";
+
 function Main() {
+  const bgImages = [tlou1, tlou2, tlou3, tlou5, tlou6];
+
+  const [currentBgIndex, setCurrentBgIndex] = useState(0);
+
+  const changeBg = () => {
+    setCurrentBgIndex((index) => (index + 1) % bgImages.length);
+    setTimeout(() => {
+      document.querySelector(".vingette").classList.add("fade-in");
+      setTimeout(() => {
+        document.querySelector(".vingette").classList.remove("fade-in");
+      }, 500);
+    }, 0);
+  };
   const backgroundStyle = {
-    backgroundImage: `url(${tlou1})`,
+    backgroundImage: `url(${bgImages[currentBgIndex]})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
-    height: "100vh", // Adjust height as needed
+    height: "100vh",
     minHeight: "100vh",
-    overflowY: "auto", // Add overflow-y auto
+    overflowY: "auto",
+    opacity: 1,
+    transition: "opacity 0.5s ease-in-out",
   };
+
   return (
-    <body style={backgroundStyle} className="vingette overflow-y-auto">
-      <nav className="flex items-center justify-center flex-wrap p-6 mb-7  ">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className="font-bold text-6xl hover:cursor-pointer">TLOU</span>
-        </div>
-
-        <div className="w-full flex-grow text-center sm:flex sm:items-center sm:w-auto ">
-          <div className="text-3xl sm:flex-grow flex ">
-            <a
-              href="./src"
-              className="block mt-4 sm:inline-block sm:mt-0 text-stone-300 hover:text-white mr-4"
-            >
-              Info
-            </a>
-            <a
-              href="./src"
-              className="block mt-4 sm:inline-block sm:mt-0 text-stone-300  hover:text-white mr-4"
-            >
-              Merch
-            </a>
-            <a
-              href="./src"
-              className="block mt-4 sm:inline-block sm:mt-0 text-stone-300 hover:text-white mr-4"
-            >
-              Credits
-            </a>
+    <body className="vingette overflow-y-auto">
+      <div className="vingette overflow-y-auto" style={backgroundStyle}>
+        <nav className="flex items-center justify-center flex-wrap p-6 mb-7  ">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            <span className="font-bold text-6xl hover:cursor-pointer">
+              TLOU
+            </span>
           </div>
-        </div>
-      </nav>
-      <div className="container h-screen flex justify-left items-center">
-        <div className="mb-80 text-stone-200 text-2xl">
-          <h1 className="text-5xl font-bold mb-5">The Last Of Us</h1>
-          <p className="fontForParagraph leading-relaxed">
-            Experience the emotional storytelling and unforgettable characters
-            <br /> in The Last of Us™, winner of over 200 Game of the Year
-            awards. <br />
-            In a ravaged civilization, where infected and hardened survivors run
-            rampant, Joel, <br /> a weary protagonist, is hired to smuggle
-            14-year-old Ellie out of a military quarantine zone. <br /> However,{" "}
-            what starts as a small job soon transforms into a brutal
-            cross-country journey.
-          </p>
-        </div>
 
-        <div className="flex justify-center items-end absolute bottom-0 p-5 mr-[-1450px]">
-          <div className="flex justify-center items-end absolute bottom-0 p-40 mr-[-1450px]">
-            <button className="paginationBtn">◉</button>
-            <button className="paginationBtn">‎</button>
-            <button className="paginationBtn">‎</button>
-            <button className="paginationBtn">‎</button>
+          <div className="w-full flex-grow text-center sm:flex sm:items-center sm:w-auto ">
+            <div className="text-3xl sm:flex-grow flex ">
+              <a
+                href="./src"
+                className="block mt-4 sm:inline-block sm:mt-0 text-stone-300 hover:text-white mr-4"
+              >
+                Info
+              </a>
+              <a
+                href="./src"
+                className="block mt-4 sm:inline-block sm:mt-0 text-stone-300  hover:text-white mr-4"
+              >
+                Merch
+              </a>
+              <a
+                href="./src"
+                className="block mt-4 sm:inline-block sm:mt-0 text-stone-300 hover:text-white mr-4"
+              >
+                Credits
+              </a>
+            </div>
           </div>
-        </div>
+        </nav>
+        <div className="container h-screen flex justify-left items-center">
+          <div className="mb-80 text-stone-200 text-2xl">
+            <h1 className="text-5xl font-bold mb-5">The Last Of Us</h1>
+            <p className="fontForParagraph leading-relaxed shadow-ms">
+              Experience the emotional storytelling and unforgettable characters
+              <br /> in The Last of Us™, winner of over 200 Game of the Year
+              awards. <br />
+              In a ravaged civilization, where infected and hardened survivors
+              run rampant, Joel, <br /> a weary protagonist, is hired to smuggle
+              14-year-old Ellie out of a military quarantine zone. <br />{" "}
+              However, what starts as a small job soon transforms into a brutal
+              cross-country journey.
+            </p>
+            <button className="py-4 px-4 rounded-xl mt-5 hover:text-stone-200 hover:bg-stone-900 text-stone-900 bg-stone-200">
+              Watch Trailer
+            </button>
+          </div>
 
-        {/* <div className="flex-column justify-center text-center items-center">
+          <div className="flex justify-center items-end absolute bottom-0 p-5 mr-[-1450px]">
+            <div className="flex justify-center items-end absolute bottom-0 p-40 mr-[-1450px]">
+              <button className="paginationBtn">◉</button>
+              <button className="paginationBtn" onClick={changeBg}>
+                ‎
+              </button>
+              <button className="paginationBtn" onClick={changeBg}>
+                ‎
+              </button>
+              <button className="paginationBtn" onClick={changeBg}>
+                ‎
+              </button>
+              <button className="paginationBtn" onClick={changeBg}>
+                ‎
+              </button>
+            </div>
+          </div>
+
+          {/* <div className="flex-column justify-center text-center items-center">
           <h1 className="text-white underline font-bold text-6xl">
             The Last Of Us
           </h1>
@@ -82,9 +119,9 @@ function Main() {
           <strong className="font-bold">CLICKER COMING</strong>
           <span className="block sm:inline">Please be aware</span>
         </div> */}
-      </div>
+        </div>
 
-      {/* <div className="max-w-sm mx-auto flex p-10 bg-white rounded-lg shadow-xl">
+        {/* <div className="max-w-sm mx-auto flex p-10 bg-white rounded-lg shadow-xl">
         <div className="flex-shrink-0">
           <img src={tlou} alt="" className="w-26 h-32" />
         </div>
@@ -95,8 +132,9 @@ function Main() {
           </p>
         </div>
       </div> */}
-      {/* <img src={tlou1} alt="" srcset="" className="z-0" /> */}
-      <Quiz />
+        {/* <img src={tlou1} alt="" srcset="" className="z-0" /> */}
+        <Quiz />
+      </div>
     </body>
   );
 }
